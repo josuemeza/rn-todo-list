3fd5c207c34c652e438d37c645361867ec11714d
Josue Meza <jmeza@Josues-MacBook-Pro-2.local>
2022-07-30 19:18:40 -0400

    fix: remove some magic numbers

--- src/data/todos.js
+++ src/data/todos.js
@@ -1,17 +1,20 @@
 import { URL_API } from "../constants/database/firebase"
 
+const DEFAULT_METHOD = "GET"
+const SERVICE_NAME = "todos"
+
 export const fetchTodos = async (params) => {
 	const {
 		method,
 		key,
 		body
 	} = {
-		method: "GET",
+		method: DEFAULT_METHOD,
 		...params
 	}
 	const singleKey = key ? `/${key}` : ""
 	const requestBody = body ? JSON.stringify(body) : undefined
-	const response = await fetch(`${URL_API}/todos${singleKey}.json`, {
+	const response = await fetch(`${URL_API}/${SERVICE_NAME}${singleKey}.json`, {
 		method,
 		headers: {
 			"Content-Type": "application/json"
